13fe5303506a2c5a1cde1247f403f1b9966bfdd7
Josh Petty <127873285+Josh-Petty@users.noreply.github.com>
2023-08-15 12:06:34 -0400

    completed app

---
+++ script.js
@@ -0,0 +1,1 @@
+ReactDOM.render(React.createElement(CurrencyConverter, null), document.getElementById('root'));
---
+++ src/script.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <CurrencyConverter />,
+  document.getElementById('root')
+);
